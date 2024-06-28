ae5cc6783b7a6439c87824cbf8646179c36ad50a
x99oly <oliveira.samuel.edu@gmail.com>
2024-06-28 17:03:27 -0300

    orderQueue buscando de pedidos, redirecionando para pedidos-retirados ou pedidos-encerrados

--- codigo/assets/script/orderQueue.js
+++ codigo/assets/script/orderQueue.js
@@ -1,42 +1,63 @@
 
 let comprasPendentes = [];
 let comprasRetiradas = [];
+let comprasEncerradas = [];
+
 const content = document.querySelector(".content");
 
-function retirarPedido(idPedido){
-    comprasPendentes = abrirLS('pedidos');
-    comprasRetiradas = abrirLS('retiradas');
+function fillParamsList() {
+    comprasPendentes = JSON.parse(localStorage.getItem('pedidos')) || []
+}
 
-    let compraFinalizada = comprasPendentes.find(p => p.compraID === idPedido);
+function retirarPedido(clienteEmail) {
+    comprasRetiradas = abrirLS('pedidos-retirados');
+
+    let compraFinalizada = comprasPendentes.find(p => p.cliente === clienteEmail);
+    if (!compraFinalizada) {
+        console.log(`Pedido do cliente ${clienteEmail} não encontrado.`);
+        return
+    }
+
     comprasRetiradas.push(compraFinalizada);
-    console.log("retirados: ", comprasRetiradas);
-    salvarLS('retiradas', comprasRetiradas);    
+    comprasRetiradas.forEach( o =>{
+        o.status = 'retirado'
+    })
+    salvarLS('pedidos-retirados', comprasRetiradas);
 
-    comprasPendentes = comprasPendentes.filter(p => p.compraID !== idPedido);
-    salvarLS('carrinho', comprasPendentes);
+    console.log("retirados: ", comprasRetiradas);
+
+    comprasPendentes = comprasPendentes.filter(p => p.cliente !== clienteEmail);
+
+    salvarLS('pedidos', comprasPendentes);
     filaDePedidos();
 }
 
-function finalizarFilaDePedidos(){
-    comprasPendentes = abrirLS('carrinho');
+
+function finalizarFilaDePedidos() {
+    comprasEncerradas = abrirLS('pedidos')
+    if(!comprasEncerradas){
+        alert('Não há pedidos na fila.')
+        return
+    }
 
     comprasPendentes = [];
 
-    salvarLS('carrinho', comprasPendentes);
+    comprasEncerradas.forEach(o => {
+        o.status = 'encerrado'
+    })
+
+    salvarLS('compras-encerradas', comprasEncerradas)
+    salvarLS('pedidos', comprasPendentes);
 
     filaDePedidos();
 }
 
 function filaDePedidos() {
-    comprasPendentes = abrirLS('carrinho');
 
     divCarrinho();
-
-    console.log("chamada!")
 }
 
 function divCarrinho() {
-
     content.innerHTML = ``;
 
     comprasPendentes.forEach(p => {
@@ -44,25 +65,25 @@ function divCarrinho() {
         div.className = 'container';
 
         let itensString = '';
-        p.carrinho.forEach((item, index) => {
-            itensString += `${item.nome} (${item.quantidade})`;
-            if (index !== p.carrinho.length - 1) {
+        p.items.forEach((item, index) => {
+            itensString += `${item.nome} (${item.qtd})`;
+            if (index !== p.items.length - 1) {
                 itensString += ', '; // Adiciona vírgula entre os itens, exceto no último
             }
         });
 
+        // Supondo que você tenha um campo de ID de compra e total no objeto p.
         div.innerHTML = `
-        <h3>Pedido: ${p.compraID}  Cliente: ${p.cliente}</h3>
+        <h3>Cliente: ${p.cliente}</h3>
         <h4>ITENS: ${itensString}</h4>
-        <p>DESCRIÇÃO DO PEDIDO: ${p.descricao}</p>
-        <h3>Total: ${p.total}</h3>
+        <h3>Total: ${p.items.reduce((acc, item) => acc + parseFloat(item.total.replace('R$', '').trim()), 0).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
 
-        <button class="btn" onClick="retirarPedido(${p.compraID})">Retirar</button>
-    
+        <button class="btn" onClick="retirarPedido('${p.cliente}')">Retirar</button>
     `;
         content.appendChild(div);
     });
 }
+
 
 function abrirLS(chave) {
     let ls = JSON.parse(localStorage.getItem(chave) || '[]');
@@ -76,7 +97,6 @@ function salvarLS(chave, item) {
     console.log('Item atribuído ao Local Storage com sucesso.');
 }
 
-
-
+setInterval(filaDePedidos, 400); 
+fillParamsList();
 filaDePedidos();
-//setInterval(filaDePedidos, 4000); 
