ceb41c47c9c4a97b8bd2cd80e96818c42da68a1f
Humberto <humberto.samora@gmail.com>
2024-06-29 04:56:59 -0300

    correção tela de perfil de usuário

--- codigo/assets/script/perfil.js
+++ codigo/assets/script/perfil.js
@@ -9,23 +9,23 @@ const defaultProfile  = {
 
   // Função para carregar o perfil do Local Storage
   function loadProfile() {
-    let storedProfile = JSON.parse(localStorage.getItem('userProfile'));
+    let storedProfile = JSON.parse(localStorage.getItem('userList'));
 
     if (storedProfile == null || Object.keys(storedProfile).length === 0) {
         storedProfile = defaultProfile;
-        localStorage.setItem('userProfile', JSON.stringify(defaultProfile));
+        localStorage.setItem('userList', JSON.stringify(defaultProfile));
     }
 
-    fillForm(storedProfile);
+    fillForm(storedProfile[0]);
 }
 
 // Função para preencher o formulário com os dados do perfil
 function fillForm(profile) {
-    document.getElementById('name').value = profile.name;
-    document.getElementById('shift').value = profile.shift;
+    document.getElementById('name').value = profile.firstName;
+    document.getElementById('shift').value = profile.lastName;
     document.getElementById('email').value = profile.email;
-    document.getElementById('phone').value = profile.phone;
-    document.getElementById('person-id').value = profile.person_id;
+    //document.getElementById('phone').value = profile.phone;
+    document.getElementById('person-id').value = profile.enrollment;
 }
 
 // Função para editar o perfil
@@ -33,7 +33,7 @@ function editProfile() {
     document.getElementById('name').removeAttribute('readonly');
     document.getElementById('shift').removeAttribute('readonly');
     document.getElementById('email').removeAttribute('readonly');
-    document.getElementById('phone').removeAttribute('readonly');
+    //document.getElementById('phone').removeAttribute('readonly');
 
     document.getElementById('edit-button').style.display = 'none';
     document.getElementById('save-button').style.display = 'inline-block';
@@ -41,16 +41,17 @@ function editProfile() {
 
 
 function saveProfile() {
-    const updatedProfile = {
-        name: document.getElementById('name').value,
-        shift: document.getElementById('shift').value,
+    let Profile = JSON.parse(localStorage.getItem('userList'))[0];
+    const updatedProfile =[ {
+        firstName: document.getElementById('name').value,
+        lastName: document.getElementById('shift').value,
         email: document.getElementById('email').value,
-        phone: document.getElementById('phone').value,
-        person_id: document.getElementById('person-id').value
-    };
+        password: Profile.password,
+        enrollment: document.getElementById('person-id').value
+    }];
 
   
-    localStorage.setItem('userProfile', JSON.stringify(updatedProfile));
+    localStorage.setItem('userList', JSON.stringify(updatedProfile));
 
     alert('Perfil salvo com sucesso!');
 
